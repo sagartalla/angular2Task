d6a9e4ace7cc2ebefb514989baf4b9742529e0bb
sagar talla <sagar.talla@zeuslearning.com>
2016-09-20 23:04:05 +0530

    setup rotues

---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent }
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
